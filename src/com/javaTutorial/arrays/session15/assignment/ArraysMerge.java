df3d1bebde907143009afde98daf10be2464574e
adapasowjanya <adapa.sowjanya@gmail.com>
2017-06-14 15:35:50 -0700

    ArrayMerge and sorting

---
+++ src/com/javaTutorial/arrays/session15/assignment/ArraysMerge.java
@@ -0,0 +1,34 @@
+package com.javaTutorial.arrays.session15.assignment;
+
+import java.util.Arrays;
+
+public class ArraysMerge {
+
+	public static void main(String[] args) {
+		int[] array1 = {12,25,9,23,0,0,0};
+		int[] array2 = {27,35,31};
+		int j=0;
+		
+		for(int i=0; i<array1.length; i++){
+			if(array1[i]==0){
+				array1[i]=array2[j];
+				j++;
+			}
+		}
+		
+		System.out.println("--------After Merging Arrays and Before Sorting--------");
+		for(int i=0;i<array1.length;i++){
+			System.out.print(array1[i] + " ");
+		}
+		System.out.println();
+		System.out.println("------After Sorting--------");
+		
+		Arrays.sort(array1);
+		
+		for(int i=0;i<array1.length;i++){
+			System.out.print(array1[i] + " ");
+		}
+		
+	}
+
+}
